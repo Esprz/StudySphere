b537ad93f23f775f17ea014e26d670ed41f7ad67
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2024-11-30 02:09:55 -0500

    Updated configuration

--- backend/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Express } from 'express';
-import cors from 'cors';
-import { Pool } from 'pg';
-
-const app: Express = express();
-
-app.use(cors());
-
---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postsRouter from './routes/posts';
+
+// Configure dotenv
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/posts', postsRouter);
+
+
+// Example route to test the server
+app.get('/', (req: Request, res: any) => {
+    res.send('Server is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
