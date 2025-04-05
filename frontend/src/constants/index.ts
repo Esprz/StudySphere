61bf8b7329878975d4e1382f496b3c268b4ccfe2
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2025-04-05 03:53:51 -0400

    refactor: setup prisma

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,14 +1,17 @@
 export const sidebarLinks = [
+  
     {
       imgURL: "/assets/icons/home.svg",
       route: "/",
       label: "Home",
     },
+        
     {
       imgURL: "/assets/icons/wallpaper.svg",
       route: "/explore",
       label: "Explore",
     },
+    /*
     {
       imgURL: "/assets/icons/people.svg",
       route: "/all-users",
@@ -19,6 +22,7 @@ export const sidebarLinks = [
       route: "/saved",
       label: "Saved",
     },
+    */
     {
       imgURL: "/assets/icons/gallery-add.svg",
       route: "/create-post",
@@ -27,21 +31,26 @@ export const sidebarLinks = [
   ];
   
   export const bottombarLinks = [
+    
     {
       imgURL: "/assets/icons/home.svg",
       route: "/",
       label: "Home",
     },
+    
+    
     {
       imgURL: "/assets/icons/wallpaper.svg",
       route: "/explore",
       label: "Explore",
     },
+    /*
     {
       imgURL: "/assets/icons/bookmark.svg",
       route: "/saved",
       label: "Saved",
     },
+    */
     {
       imgURL: "/assets/icons/gallery-add.svg",
       route: "/create-post",
