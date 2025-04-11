0d1d2d4f4e757099d8339a4f7d675168c3c66a2e
ESPRZ <siyuan.zhao.syesprz@gmail.com>
2025-04-11 01:14:49 -0400

    refactor: change color of search input box

--- frontend/src/_root/pages/Explore.tsx
+++ frontend/src/_root/pages/Explore.tsx
@@ -33,7 +33,7 @@ const Explore = () => {
 
       <div className="explore-inner_container">
         <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
-        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-2">
           <img src="/assets/icons/search.svg" width={24} height={24} alt="search" />
           <Input type="text"
             placeholder='Search'
@@ -44,14 +44,15 @@ const Explore = () => {
       </div>
 
       <div className="w-full max-w-5xl mt-16 mb-7">
-
-        <div className="flex justify-end mb-4">
+      {
+      /*
+      <div className="flex justify-end mb-4">
           <div className="flex gap-3 bg-dark-3 rounded-xl px-4 py-2 cusror-pointer">
             <p className="small-medium md:base-medium text-light-2">All</p>
             <img src="/assets/icons/filter.svg" alt="filter" width={20} />
           </div>
         </div>
-
+      */}
         <div className="flex flex-wrap gap-9 w-full max-w-5xl">
           {shouldShowSearchResult
             ? (<SearchResults isSearchFetching={isSearchFetching} searchedPosts={searchedPosts} />)
